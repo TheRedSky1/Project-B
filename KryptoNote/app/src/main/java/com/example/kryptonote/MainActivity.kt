5af3b5063f5967babde567c19f571ad92d7b4f58
TheRedSky1 <79380963+TheRedSky1@users.noreply.github.com>
2022-07-25 21:58:04 -0400

    Add files via upload

---
+++ KryptoNote/app/src/main/java/com/example/kryptonote/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.kryptonote
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    private String makePad(String note){
+
+    }
+
+}
